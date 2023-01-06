72cf0432df5305fe2b96c7cf060d31d96f2251ee
mason <masonlee7629@gmail.com>
2023-01-06 17:47:47 +0900

    test commit

---
+++ server/src/main/java/com/server/seb41_main_11/Seb41Main11Application.java
@@ -0,0 +1,13 @@
+package com.server.seb41_main_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb41Main11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb41Main11Application.class, args);
+	}
+
+}
