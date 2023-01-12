66dbf5a590ca912139489dc788a232796ef03231
mason <masonlee7629@gmail.com>
2023-01-12 20:34:05 +0900

    Fix : ProgramService @Builder 어노테이션 삭제

--- server/src/main/java/com/server/seb41_main_11/domain/program/service/ProgramService.java
+++ server/src/main/java/com/server/seb41_main_11/domain/program/service/ProgramService.java
@@ -1,18 +1,15 @@
 package com.server.seb41_main_11.domain.program.service;
 
-import com.server.seb41_main_11.domain.program.dto.ProgramDto;
 import com.server.seb41_main_11.domain.program.entity.Program;
 import com.server.seb41_main_11.domain.program.repository.ProgramRepository;
 import com.server.seb41_main_11.global.error.ErrorCode;
 import com.server.seb41_main_11.global.error.exception.BusinessException;
 import java.util.Optional;
-import lombok.Builder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-@Builder
 public class ProgramService {
     private final ProgramRepository programRepository;
 
