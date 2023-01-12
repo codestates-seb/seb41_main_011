453485eab965b0e714936c3967fa540f658c4289
gkgjswls <gkgjswls842@gmail.com>
2023-01-12 17:30:56 +0900

    Desgin: 로그인 경로 라우트 추가
    hover이펙트 추가 width 뷰포트기준으로 변환

--- client/src/components/ButtonAccept.tsx
+++ client/src/components/ButtonAccept.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 
 const AcceptButton = styled.button`
-    width: 280px;
+    width: 80vw;
     height: 40px;
     border-radius: 12px;
     font-size: 1.25rem;
@@ -10,6 +10,13 @@ const AcceptButton = styled.button`
     background-color: #F4E7A4;
     border: none;
     box-shadow: #af9a7052 0px 3px 8px;
+    &:hover{
+      opacity: 80%;
+
+    }
+    @media screen and (min-width: 768px) {
+      width: 600px;
+    }
 `
 
 type ButtonAcceptProps ={
