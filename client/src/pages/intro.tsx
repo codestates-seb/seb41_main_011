dab06722cce208019a32a59c2ffbe0a3d2ee6ecb
ch0ee <choco920@gmail.com>
2023-01-22 16:27:49 +0900

    [Design] 상하여백 조정

--- client/src/pages/intro.tsx
+++ client/src/pages/intro.tsx
@@ -1,157 +1,144 @@
-import { useNavigate } from "react-router";
-import styled from "styled-components";
-import Tabbar from "../components/tabbar"; //탭바 테스트용. 해당 부분 삭제 예정
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const OuterWrapper = styled.div`
-	height: 100vh;
+  min-height: calc(100vh - 120px);
   display: flex;
-	flex-direction: row;
-	justify-content: center;
-	align-items: center;
-	@media screen and (max-width: 768px) {
-		height: 100vh;
-        flex-direction: column-reverse;
-    }
-`
+  justify-content: center;
+  align-items: flex-start;
+  gap: 5vw;
+  width: 100%;
+  padding: 60px 20px;
+
+  @media screen and (min-width: 1200px) {
+    flex-direction: row;
+    align-items: center;
+    padding: 0;
+    min-height: 100vh;
+  }
+`;
 
 const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  width: 100%;
 
-	height: 100vh;
-	/* border: 1px solid green; */
-    display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		* {
-			margin-bottom: 10%;
-		}
-`
+  @media screen and (min-width: 1200px) {
+    width: 400px;
+  }
+`;
 
 const MainMessage = styled.div`
-	margin-top: 20px;
-	color: #4B6A4D;
-	font-weight: 700;
-	font-size: 2.25rem;
-`
+  color: #4b6a4d;
+  font-weight: 700;
+  font-size: 2.25rem;
+  text-align: center;
+  width: 100%;
+  @media screen and (min-width: 1200px) {
+    text-align: left;
+    width: 400px;
+  }
+`;
 
-const SubMessage = styled.div` 
-	@media screen and (min-width: 768px) {
-		font-size: 14px;
+const SubMessage = styled.div`
+  width: 100%;
+  text-align: center;
+  font-size: 1.1rem;
+  color: #333;
+
+  strong {
+    font-weight: 500;
   }
 
-	@media screen and (min-width: 1200px) {
-		font-size: 16px;
-  }   
-`
+  @media screen and (min-width: 1200px) {
+    width: 400px;
+    text-align: left;
+    margin-bottom: 12px;
+  }
+`;
 
 const ButtonWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	* {
-			margin: 10px;
-	}
-`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 const Button = styled.button`
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 3px 4px;
+  border-radius: 10px;
+  border: none;
+  height: 3em;
+  width: 100%;
+  background-color: #c4dcbf;
+  color: #333;
+  font-size: 1rem;
+  cursor: pointer;
 
-	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-	border-radius: 10px;
-	border: none;
-	height: 3em;
-	width: 20em;
-	background-color: #C4DCBF;
-	font-weight: 500;
-	color: #3d553e;
-	font-size: 1rem;
-	&:hover {
-			background-color: #70846C;
-			color: white;
-			cursor: pointer;
-	}
-
-	@media screen and (min-width: 768px) {
-		height: 3em;
-		width: 35vw;
-		font-size: 14px;
+  &:hover {
+    background-color: #70846c;
+    color: white;
   }
 
-	@media screen and (min-width: 1200px) {
-		height: 3em;
-		width: 400px;
-		font-size: 16px;
+  a {
+    color: inherit;
+    display: block;
   }
 
-`
+  @media screen and (min-width: 768px) {
+    width: 400px;
+    margin: 0 auto;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 320px;
+    margin: 0;
+  }
+`;
 const Logo = styled.img`
-	width: 200px;
-	height: 200px;
-    /* margin-bottom: 100px; */
-	margin-left: 5vw;
-	@media screen and (max-width: 768px) {
-        display: none;
+  width: 320px;
+  height: 320px;
+  @media screen and (max-width: 768px) {
+    display: none;
   }
-	@media screen and (min-width: 768px) {
-		width: 300px;
-		height: 300px;
-		margin-bottom: 40px;
-  }
-	@media screen and (min-width: 1200px) {
-		width: 400px;
-		height: 400px;
-		margin-bottom: 40px;
-  }
-`
+`;
 
 const MobileLogo = styled.img`
-    margin-left: 30px;
-	width: 20em;
-	@media screen and (min-width: 768px) {
-		display: none;
-    }
-`
+  width: 200px;
+  @media screen and (min-width: 1200px) {
+    width: 280px;
+  }
+  @media screen and (min-width: 1200px) {
+    display: none;
+  }
+`;
 
 const Intro = () => {
+  return (
+    <div>
+      <OuterWrapper>
+        <ContentWrapper>
+          <MainMessage>안녕하세요! 반가워요</MainMessage>
+          <SubMessage>
+            시작하기 전 간단한 질문이 있어요.
+            <br />
+            <strong>그룹 테라피</strong>에 대해 친숙하신가요?😊
+          </SubMessage>
+          <MobileLogo src='green-tea.png' />
+          <ButtonWrapper>
+            <Button>
+              <Link to='/about'>잘 몰라요. 조금 더 알려주실래요?</Link>
+            </Button>
+            <Button>
+              <Link to='/'>이미 알고있어요</Link>
+            </Button>
+          </ButtonWrapper>
+        </ContentWrapper>
 
-	const navigate = useNavigate();
-	const toAboutPage = () => {
-		navigate('/about')
-	}
-
-	return (
-		<div>
-			<OuterWrapper>
-					{/* <Tabbar />  */}
-
-
-				{/* 탭바 테스트용. 위 라인 삭제 예정 */}
-				<ContentWrapper>
-					
-					<MainMessage>
-							안녕하세요! 반가워요
-					</MainMessage>
-					<SubMessage>
-					시작하기 전 간단한 질문이 있어요.<br/>
-					그룹 테라피에 대해 친숙하신가요?😊
-					</SubMessage>
-					<MobileLogo src="green-tea.png" />
-					<ButtonWrapper>
-							<Button onClick={() => toAboutPage()}>
-									잘 몰라요. 조금 더 알려주실래요?
-							</Button>
-							<Button>
-									이미 알고있어요
-							</Button>
-					</ButtonWrapper>
-					
-					
-				</ContentWrapper>
-
-				<Logo src="green-tea.png"></Logo>
-					
-			</OuterWrapper>
-		</div>
-	)
+        <Logo src='green-tea.png'></Logo>
+      </OuterWrapper>
+    </div>
+  );
 };
 
 export default Intro;
