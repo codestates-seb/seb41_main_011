c15ebeb0593b848359ecac172eb2c22c2c196db8
treewww <replicaset02@gmail.com>
2023-01-23 19:05:29 +0900

    feat: "Fix Scripts"
    Co-authored-by: roal <110204732+replicaset01@users.noreply.github.com>

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -24,6 +24,22 @@ else
 fi
 
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /root/main/back/log/deploy.log
-./root/main/back/scripts/start.sh 2>/root/main/back/deploy_err.log &
+# EC2 내 Spring Boot 실행 스크립트
+
+# 서버가 실행중이면 프로세스 종료
+ps -ef | grep "main_11-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $2}' | xargs kill -9 2> /dev/null
+
+# 종료 이력 파악 후 적절한 문구 출력
+if [ $? -eq 0 ];then
+    echo "Successfully Stopped"
+else
+    echo "Application is Not Running"
+fi
+
+# 어플리케이션 재 실행
+echo "Application Restarted!"
+echo $1
+
+# 어플리케이션 백그라운드 실행
+nohup java -jar /root/main/back/build/libs/main_11-0.0.1-SNAPSHOT.jar 2> /root/main/back/depoly.log &
