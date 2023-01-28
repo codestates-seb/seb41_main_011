c508405962ed2ecceffdb8bfabcdc844fad432a9
ch0ee <choco920@gmail.com>
2023-01-29 02:14:10 +0900

    [Feat] 일반회원 마이페이지

--- client/src/pages/my_p_detail_general.tsx
+++ client/src/pages/my_p_detail_general.tsx
@@ -1,10 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Button from '../components/UI/Button';
 import Header from '../components/Header';
 import Tabbar from '../components/tabbar';
 import Footer from '../components/Footer';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+  calculateStatus,
+  viewBookDate,
+  viewCost,
+  viewProgramDate,
+} from '../utils';
 
 const Contents = styled.main`
   width: 100%;
@@ -43,21 +51,26 @@ const Detail = styled.div`
 
   ul li {
     font-weight: 300;
-    margin-top: 4px;
+    margin-top: 8px;
 
     strong {
       font-weight: 500;
       color: #72ab76;
-      margin-right: 8px;
+      margin-right: 16px;
     }
 
     p {
-      margin-top: 2px;
+      margin-top: 6px;
     }
 
-    a {
+    .cancel {
+      border: none;
+      background: none;
+      font-size: 1rem;
+      padding: 0;
       color: #8e6610;
       font-weight: 400;
+      cursor: pointer;
       &:hover {
         text-decoration: underline;
       }
@@ -85,6 +98,7 @@ const ButtonWrapper = styled.div`
 
   a {
     color: inherit;
+    display: block;
   }
 `;
 
@@ -112,25 +126,80 @@ const Status = styled.div`
 `;
 
 const MyProgramDetailG = () => {
+  const payId = useParams().id;
+  const [programInfo, setProgramInfo] = useState({
+    payId: 0,
+    createdAt: '',
+    status: '',
+    programId: 0,
+    title: '',
+    dateStart: '',
+    dateEnd: '',
+    userMax: 0,
+    cost: 0,
+    zoomLink: '',
+    announce: '',
+    counselorName: '',
+  });
+
+  const getProgramInfo = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_DB_HOST + `/api/pays/lookup/${payId}`,
+      );
+      setProgramInfo(response.data.data);
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProgramInfo();
+  }, [payId]);
+
+  const patchPayment = async () => {
+    try {
+      await axios.patch(
+        process.env.REACT_APP_DB_HOST + `/api/pays/lookup/${payId}/edit`,
+      );
+      alert('취소 신청이 완료 되었습니다.');
+      window.location.reload();
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  const cancelBookHandler = () => {
+    const confirmCancel = window.confirm('프로그램 예약을 취소할까요?');
+    if (confirmCancel) {
+      patchPayment();
+    }
+  };
+
   return (
     <div>
       <Header />
       <Contents>
-        <Status>진행예정</Status>
-        <Title>
-          프로그램 제목 어쩌구 저쩌구 프로그램 제목 어쩌구 저쩌구 프로그램 제목
-          어쩌구
-        </Title>
+        <Status>
+          {calculateStatus(
+            programInfo.status,
+            programInfo.dateStart,
+            programInfo.dateEnd,
+          )}
+        </Status>
+        <Title>{programInfo.title}</Title>
         <Detail>
           <ul>
             <li>
-              <strong>상담사</strong>햄토끼
+              <strong>상담사</strong>
+              {programInfo.counselorName}
             </li>
             <li>
-              <strong>일정</strong>2023년 0월 00일 3:30PM ~ 4:30PM
+              <strong>일정</strong>
+              {viewProgramDate(programInfo.dateStart, programInfo.dateEnd)}
             </li>
             <li>
-              <strong>정원</strong>최대 10인
+              <strong>정원</strong>최대 {programInfo.userMax}인
             </li>
           </ul>
         </Detail>
@@ -140,15 +209,23 @@ const MyProgramDetailG = () => {
           <ul>
             <li>
               <strong>그룹 상담 접속 링크</strong>
-              <p>
-                <a href='https://google.com' target='blank'>
-                  https://google.com
-                </a>
-              </p>
+              {programInfo.zoomLink ? (
+                <p>
+                  <a href={programInfo.zoomLink} target='blank'>
+                    {programInfo.zoomLink}
+                  </a>
+                </p>
+              ) : (
+                <p>접속 링크는 상담 시작 20분 전까지 전달됩니다.</p>
+              )}
             </li>
             <li>
               <strong>참여자 전달 사항</strong>
-              <p>프로그램 시작 5분 전까지 상담실로 입장해주세요.</p>
+              {programInfo.announce ? (
+                <p>{programInfo.announce}</p>
+              ) : (
+                <p>전달사항이 없습니다.</p>
+              )}
             </li>
           </ul>
         </Detail>
@@ -157,26 +234,40 @@ const MyProgramDetailG = () => {
           <h4>예약 정보</h4>
           <ul>
             <li>
-              <strong>예약일시</strong>2023년 1월 2일 3:21PM
+              <strong>예약일시</strong>
+              {viewBookDate(programInfo.createdAt)}
             </li>
             <li>
-              <strong>결제금액</strong>30,000원
+              <strong>결제금액</strong>
+              {viewCost(programInfo.cost)}원
             </li>
             <li>
               <strong>결제방법</strong>신용카드
             </li>
             <li>
               <strong>예약취소</strong>
-              <Link to='#'>취소신청</Link>
-              <span className='tip'>
-                * 예약 취소는 일정 시작 48시간 전까지만 가능합니다.
-              </span>
+              {programInfo.status === '결제 완료' ? (
+                <>
+                  <button
+                    className='cancel'
+                    type='button'
+                    onClick={cancelBookHandler}
+                  >
+                    취소신청
+                  </button>
+                  <span className='tip'>
+                    * 예약 취소는 일정 시작 48시간 전까지만 가능합니다.
+                  </span>
+                </>
+              ) : (
+                programInfo.status
+              )}
             </li>
           </ul>
         </Detail>
         <ButtonWrapper mgt='4px'>
           <Button width='100%' height='3em' fontsize='1rem'>
-            <Link to='/myprogram'>목록보기</Link>
+            <Link to='/mypage'>목록보기</Link>
           </Button>
         </ButtonWrapper>
       </Contents>
