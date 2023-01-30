7938eafa1ba3e80fdedf2c188723c352eb5e98cd
ch0ee <choco920@gmail.com>
2023-01-30 14:36:42 +0900

    [Feat] 게시글 0개일 때 메세지

--- client/src/pages/CommunityMain.tsx
+++ client/src/pages/CommunityMain.tsx
@@ -76,6 +76,13 @@ const MessageGrid = styled.div`
     }
   }
 `;
+const Empty = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #ddd;
+  padding: 50px 0;
+  text-align: center;
+`;
+
 const Post = styled.div`
   display: grid;
   grid-template-columns: 8% 80% 8%;
@@ -92,6 +99,7 @@ const Post = styled.div`
     justify-content: center;
     align-items: center;
   }
+
   .wrapper {
     max-width: 100%;
     padding: 0.4rem 0;
@@ -305,19 +313,36 @@ const CommunityMain = (props: any) => {
               )}
         </MessageGrid>
         <PostWRapper>
-          {postList.map((post: any) => {
-            return (
-              <Post>
-                <div className='postIndex'>
-                  {isNotice ? post.noticeId : post.postId}
-                </div>
-                <div className='wrapper'>
-                  <div className='postTitle'>
-                    {isNotice ? (
-                      <Tag>공지</Tag>
-                    ) : (
-                      <Tag>{viewBoardCategory(post.kinds)}</Tag>
-                    )}
+          {postList.length !== 0 ? (
+            postList.map((post: any) => {
+              return (
+                <Post>
+                  <div className='postIndex'>
+                    {isNotice ? post.noticeId : post.postId}
+                  </div>
+                  <div className='wrapper'>
+                    <div className='postTitle'>
+                      {isNotice ? (
+                        <Tag>공지</Tag>
+                      ) : (
+                        <Tag>{viewBoardCategory(post.kinds)}</Tag>
+                      )}
+                      <Link
+                        to={
+                          isNotice
+                            ? `/community/notice/${post.noticeId}`
+                            : `/community/general/${post.postId}`
+                        }
+                      >
+                        {isNotice ? post.title : post.title}
+                      </Link>
+                    </div>
+                    <div className='postInfo'>
+                      {viewBookDate(post.createdTime)} · {post.writer}
+                    </div>
+                  </div>
+
+                  <div className='postButton'>
                     <Link
                       to={
                         isNotice
@@ -325,28 +350,15 @@ const CommunityMain = (props: any) => {
                           : `/community/general/${post.postId}`
                       }
                     >
-                      {isNotice ? post.title : post.title}
+                      +
                     </Link>
                   </div>
-                  <div className='postInfo'>
-                    {viewBookDate(post.createdTime)} · {post.writer}
-                  </div>
-                </div>
-
-                <div className='postButton'>
-                  <Link
-                    to={
-                      isNotice
-                        ? `/community/notice/${post.noticeId}`
-                        : `/community/general/${post.postId}`
-                    }
-                  >
-                    +
-                  </Link>
-                </div>
-              </Post>
-            );
-          })}
+                </Post>
+              );
+            })
+          ) : (
+            <Empty>아직 {isNotice ? '공지사항' : '게시글'}이 없습니다.</Empty>
+          )}
         </PostWRapper>
         <PaginationWrapper>
           <Pagination
