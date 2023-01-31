c12db401d905b6da6849e9c2e37e57ce0fe81e2c
jungdonggyu <1013as@naver.com>
2023-01-31 10:01:24 +0900

    fix : "카카오 로그인 응답 방식 수정"

--- server/src/main/java/com/server/seb41_main_11/web/kakaotoken/controller/KakaoTokenController.java
+++ server/src/main/java/com/server/seb41_main_11/web/kakaotoken/controller/KakaoTokenController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -31,13 +33,13 @@ public class KakaoTokenController {
     }
     // login 페이지에서 카카오 로그인 버튼 클릭 시 -> oauth/kako/callback으로 리다이렉트(개발자 사이트에서 직접 설정함)
     // loginCallback 메서드 실행 -> reqeustKakaoToken 메서드에서 FeignClient 실행
-    // -> 카카오 REST API의 토큰 받기(/oauth/token) (Host: kauth.kakao.com)로 연결 -> 토큰 받아와서 kakoToken에 저장
+    // -> 카카오 REST API의 토큰 받기(/oauth/token) (Host: kauth.kakao.com)로 연결 -> 토큰 받아와서 kakaoToken에 저장
     // token_type , access_token , expires_in, refresh_token, expires_in,
     // scope(account_email, profile_image, profile_nickname) 출력
     @GetMapping("/oauth/kakao/callback")
     // redirect uri를 처리할 메서드
 //    public @ResponseBody String loginCallback(String code)
-    public ResponseEntity<KakaoTokenDto.Response> loginCallback(String code) {
+    public ResponseEntity<KakaoTokenDto.Response> loginCallback(@RequestParam String code) {
         String contentType = "application/x-www-form-urlencoded;charset=utf-8";
         KakaoTokenDto.Request kakaoTokenRequestDto = KakaoTokenDto.Request.builder()
                 .client_id(clientId)
